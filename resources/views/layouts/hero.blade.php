dee3ce35cc2876007a485a49c85bc970a55cea13
SifahuwuHia <hia.sifahuwu@gmail.com>
2024-06-18 12:07:58 +0700

    pembaharuan form

--- resources/views/layouts/hero.blade.php
+++ resources/views/layouts/hero.blade.php
@@ -7,48 +7,47 @@
     <div>
         <h1 class="text-6xl font-bold mb-5">Hai, Pencari Ilmu Sejati!</h1>
         <h2 class="text-6xl font-bold mb-8">Time To Grow Up 🙌</h2><br>
-        <p class="text-xl mb-8">Kami siap membantu kamu dalam proses belajar <br> dengan menyediakan metode yang efektif dan efisien,<br> sehingga kamu dapat menguasai materi dengan lebih mudah dan cepat,<br> serta mencapai hasil yang optimal dalam waktu yang lebih singkat!</p>
-        <button class="bg-blue-600 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-600 hover:text-white">Mulai Belajar</button>
+        <p class="text-xl mb-8">Kami siap membantu kamu dalam proses belajar dengan menyediakan <br> metode yang efektif dan efisien, sehingga kamu dapat menguasai <br> materi dengan lebih mudah dan cepat,  serta mencapai hasil <br> yang optimal dalam waktu yang lebih singkat!</p>
+        <a href="{{ route('login') }}" class="bg-blue-600 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-600 hover:text-white">Mulai Belajar</a>
     </div>
     <div class="welcome-image">
         <img src="{{asset('assets/images/hero.jpg')}}" alt="gambar hero" width="550" height="550">
     </div>
-    </div>
-    <div class="py-12">
-        <div class="max-w-6xl mx-auto text-center">
+</div>
+    <div class="py-12 mt-4">
+        <div class="max-w-6xl mx-auto text-center mb-4">
             <h2 class="text-4xl font-bold mb-4">Perhatian Kami adalah Pendidikan Anda</h2>
             <p class="text-lg mb-8">Kami membantu orang membangun masa depan yang sukses dan mencapai kebebasan finansial <br> untuk melakukan apa yang mereka inginkan, dimanapun yang mereka inginkan</p>
         </div><br>
 
         <div class="max-w-6xl mx-auto grid grid-rows-3 grid-flow-col gap-8 justify-center">
-            <div class="flex flex-col items-center md:flex-row md:col-span-1">
-                <div class="w-full md:w-1.5/3">
-                    <img src="{{ asset('assets/images/minat.jpg
-                    ') }}" alt="Education Image 1" class="rounded-lg mb-4 md:mb-0 md:mr-4" >
+            <div class="flex flex-col items-center md:flex-row md:col-span-1 mb-8 mt-5">
+                <div class="w-full md:w-1.5/3 flex items-center justify-center">
+                    <img src="{{ asset('assets/images/minat.jpg') }}" alt="Education Image 1" class="rounded-lg mb-4 md:mb-0 md:mr-4" >
                 </div>
                 <div class="w-full md:w-1.5/3">
-                    <h3 class="text-xl font-bold mb-2">01 Tentukan Minatmu</h3>
-                    <p class="text-gray-600 ml-7">Disini kami berkomitmen untuk memberikan pendidikan terbaik bagi kalian. Mulai perjalanan belajarmu dengan menentukan minatmu sejak awal, baik itu di bidang Bahasa, IPA, atau IPS. Melalui platform pembelajaran yang disesuaikan, kamu akan mendapatkan pengalaman belajar yang efektif dan efisien untuk mempersiapkan masa depan yang gemilang.</p>
-                </div>
-            </div>
-
-            <div class="flex flex-col items-center justify-items-center justify-center md:flex-row md:col-span-1">
-                <div class="w-full md:w-1.5/3">
-                    <img src="{{ asset('assets/images/inovasi.jpg') }}" alt="Education Image 2" class="rounded-lg mb-4 md:mb-0 md:mr-4 md:ml-40">
-                </div>
-                <div class="w-full md:w-1.5/3">
-                    <h3 class="text-xl font-bold mb-2">02 Berinovasi dan Menstimulasi Diskusi</h3>
-                    <p class="text-gray-600 ml-7">Disini kamu didorong untuk berpikir kreatif dan kritis. Platform ini dirancang untuk memfasilitasi inovasi dan menstimulasi diskusi, memungkinkan kamu untuk berkolaborasi, bertukar ide, dan memecahkan masalah bersama.</p>
+                    <h3 class="text-xl font-bold mb-2">Tentukan Minatmu</h3>
+                    <p class="text-gray-600 ml-7" style="margin: 0px">Disini kami berkomitmen untuk memberikan pendidikan terbaik bagi kalian. Mulai perjalanan belajarmu dengan menentukan minatmu sejak awal, baik itu di bidang Bahasa, IPA, atau IPS. Melalui platform pembelajaran yang disesuaikan, kamu akan mendapatkan pengalaman belajar yang efektif dan efisien untuk mempersiapkan masa depan yang gemilang.</p>
                 </div>
             </div>
 
-            <div class="flex flex-col items-center md:flex-row md:col-span-1">
+            <div class="flex flex-col items-center justify-items-center justify-center md:flex-row md:col-span-1 mb-5 mt-5">
                 <div class="w-full md:w-1.5/3">
+                    <h3 class="text-xl font-bold mb-2">Berinovasi dan Menstimulasi Diskusi</h3>
+                    <p class="text-gray-600 ml-7" style="margin: 0px">Disini kamu didorong untuk berpikir kreatif dan kritis. Platform ini dirancang untuk memfasilitasi inovasi dan menstimulasi diskusi, memungkinkan kamu untuk berkolaborasi, bertukar ide, dan memecahkan masalah bersama.</p>
+                </div>
+                <div class="w-full md:w-1.5/3 flex items-center justify-center">
+                    <img src="{{ asset('assets/images/inovasi.jpg') }}" alt="Education Image 2" class="rounded-lg mb-4 md:mb-0 md:mr-4 md:ml-4 0">
+                </div>
+            </div>
+
+            <div class="flex flex-col items-center md:flex-row md:col-span-1 mb-5 mt-5">
+                <div class="w-full md:w-1.5/3 flex items-center justify-center">
                     <img src="{{ asset('assets/images/feedback.jpg') }}" alt="Education Image 3" class="rounded-lg mb-4 md:mb-0 md:mr-4">
                 </div>
                 <div class="w-full md:w-1.5/3 justify-items-end">
-                    <h3 class="text-xl font-bold mb-2">03 Minta Bantuan dan Umpan Balik</h3>
-                    <p class="text-gray-600 ml-7">Kami memahami bahwa setiap siswa memiliki kebutuhan belajar yang unik. Disini, siswa dapat dengan mudah meminta bantuan dan mendapatkan umpan balik dari guru dan teman.</p>
+                    <h3 class="text-xl font-bold mb-2">Minta Bantuan dan Umpan Balik</h3>
+                    <p class="text-gray-600 ml-7 " style="margin: 0px">Kami memahami bahwa setiap siswa memiliki kebutuhan belajar yang unik. Disini, siswa dapat dengan mudah meminta bantuan dan mendapatkan umpan balik dari guru dan teman.</p>
                 </div>
             </div>
         </div>
@@ -57,7 +56,6 @@
     <div class="py-12">
         <div class="max-w-6xl mx-auto">
             <h2 class="text-3xl font-bold mb-4">Karya-Karya Siswa</h2>
-
             <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
                 @foreach ($portofolios as $portofolio)
                     <div class="bg-white rounded-lg shadow-md p-6">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
       <!-- Navbar -->
-      <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="true">
+      <nav class="sticky top-0 relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="true">
         <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
           <nav>
             <!-- breadcrumb -->
